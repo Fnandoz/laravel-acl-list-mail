dfc489fcb2a0621ad6e9e696cd05b67e59d47f95
Fernando Gomes <luis.sales@sead.pa.gov.br>
2018-01-25 11:46:15 -0300

    Frontend update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!--Let browser know website is optimized for mobile-->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/lista/new.blade.php
+++ resources/views/lista/new.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 @section('content')
-
-<form action="/home/lista/new" method="POST" enctype="multipart/form-data">
-  {{csrf_field()}}
-  <input type="text" placeholder="Titulo" name="titulo">
-  <input type="text" placeholder="Descricao" name="descricao">
-  <br>Público<input type="checkbox" name="publico">
-  <br>Imagem<input type="file" name="photo" id="photo">
-  PDF<input type="file" name="pdf" id="pdf">
-  <input type="submit" value="Salvar">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+              <form action="/home/lista/new" method="POST" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <input type="text" placeholder="Titulo" name="titulo" class="form-control">
+                <input type="text" placeholder="Descricao" name="descricao" class="form-control">
+                Público<input type="checkbox" name="publico">
+                <br>Imagem<input type="file" name="photo" id="photo">
+                PDF<input type="file" name="pdf" id="pdf">
+                <input type="submit" value="Salvar" class="form-control">
+            </div>
+      </div>
+    </div>
+  </div>
 
 @stop
